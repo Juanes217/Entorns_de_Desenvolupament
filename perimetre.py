ca9543db2a3ef1c22c2e9bdc69d060f8c119b3ee
NilGm <nilgarciam19@gmail.com>
2026-05-05 16:40:28 +0200

    actualizado

--- perimetre.py
+++ perimetre.py
@@ -1,10 +1,10 @@
 import math
 
 def perimetro_cuadrado(lado):
-    return 4 + lado
+    return 4 * lado
 
 def perimetro_circulo(radio):
     return 2 * math.pi * radio
 
 def perimetro_triangulo(lado1, lado2, lado3):
-    return lado1 + lado2 + lado3s
+    return lado1 + lado2 + lado3
