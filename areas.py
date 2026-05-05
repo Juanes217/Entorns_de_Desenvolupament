f0b57ce0d02f9c0f01868d51270dcbad5ddbe6df
Juanes217 <meraagudeloj@gmail.com>
2026-05-05 16:59:18 +0200

    formulas

---
+++ areas.py
@@ -0,0 +1,10 @@
+import math
+
+def area_cuadrado(lado):
+    return lado * lado
+
+def area_circulo(radio):
+    return math.pi * (radio ** 2)
+
+def area_triangulo(base, altura):
+    return (base * altura) / 2
